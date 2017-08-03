f0444a3eb715ccf7d03976d400f3cee12ae067ba
atwwei <atwwei@gmail.com>
2017-08-03 16:38:57 +0800

    export

---
+++ directive.ts
@@ -0,0 +1,5 @@
+export { DfpAdDirective } from './dfp-ad.directive';
+export { DfpSizeDirective } from './dfp-size.directive';
+export { DfpTargetingDirective } from './dfp-targeting.directive';
+export { DfpExclusionDirective } from './dfp-exclusion.directive';
+export { DfpValueDirective } from './dfp-value.directive';
