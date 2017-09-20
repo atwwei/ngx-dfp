82f1221b8cea891514f5ba515f10679505510e0f
atwwei <atwwei@gmail.com>
2017-09-20 10:35:18 +0800

    Delete moved files

--- directive.ts
+++
@@ -1,6 +0,0 @@
-export { DfpAdDirective } from './dfp-ad.directive';
-export { DfpAdResponsive } from './dfp-ad-responsive.directive';
-export { DfpSizeDirective } from './dfp-size.directive';
-export { DfpTargetingDirective } from './dfp-targeting.directive';
-export { DfpExclusionDirective } from './dfp-exclusion.directive';
-export { DfpValueDirective } from './dfp-value.directive';
