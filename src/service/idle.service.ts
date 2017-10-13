ea63192eeb719350adeb5ef0c8bfb71aeb934c48
atwwei <atwwei@gmail.com>
2017-10-13 08:50:58 +0800

    Add IdleService for lazyload

---
+++ src/service/idle.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, NgZone } from '@angular/core';
+
+@Injectable()
+export class IdleService {
+
+  private requestIdleCallback: any;
+
+  constructor(private zone: NgZone) {
+    let win = window as any;
+    if (win.requestIdleCallback) {
+      this.requestIdleCallback = (fun) => {
+        return win.requestIdleCallback(fun);
+      };
+    } else {
+      this.requestIdleCallback = (fun) => {
+        return zone.runOutsideAngular(() => win.setTimeout(fun, 5));
+      };
+    }
+  }
+
+  request(fun) {
+    this.requestIdleCallback(fun);
+  }
+
+}
