267579f5bf23181c2da28b1446f8659c51457cff
atwwei <atwwei@gmail.com>
2018-05-12 11:07:05 +0800

    Add config option : onSameNavigation

--- src/class/dfp-config.class.ts
+++ src/class/dfp-config.class.ts
@@ -1,16 +1,17 @@
 export class DfpTargeting {
-    [key: string]: Array<string>;
+  [key: string]: Array<string>;
 }
 
 export class DfpConfig {
-    idleLoad?: boolean;
-    singleRequestMode?: boolean;
-    collapseIfEmpty?: boolean;
-    centering?: boolean;
-    location?: string | Array<string>;
-    ppid?: string;
-    globalTargeting?: DfpTargeting;
-    forceSafeFrame?: boolean;
-    safeFrameConfig?: object;
-    loadGPT?: boolean;
+  idleLoad?: boolean;
+  onSameNavigation?: 'refresh' | 'ignore';
+  singleRequestMode?: boolean;
+  collapseIfEmpty?: boolean;
+  centering?: boolean;
+  location?: string | Array<string>;
+  ppid?: string;
+  globalTargeting?: DfpTargeting;
+  forceSafeFrame?: boolean;
+  safeFrameConfig?: object;
+  loadGPT?: boolean;
 }
