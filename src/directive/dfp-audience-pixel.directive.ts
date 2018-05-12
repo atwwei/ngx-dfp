5b5287a0e9f15504ab055d95b6cadbe6030424c3
atwwei <atwwei@gmail.com>
2018-05-12 11:06:10 +0800

    Check platform

--- src/directive/dfp-audience-pixel.directive.ts
+++ src/directive/dfp-audience-pixel.directive.ts
@@ -1,8 +1,11 @@
 import {
   Directive, ElementRef,
   Input,
-  OnInit
+  OnInit,
+  Inject,
+  PLATFORM_ID
 } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Directive({
   selector: 'dfp-audience-pixel'
@@ -14,35 +17,37 @@ export class DfpAudiencePixelDirective implements OnInit {
   @Input() ppid: number;
 
   constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
     private elementRef: ElementRef
   ) { }
 
   ngOnInit() {
-    const axel = Math.random(),
-      random = axel * 10000000000000;
+    if (isPlatformBrowser(this.platformId)) {
+      const axel = Math.random(),
+        random = axel * 10000000000000;
 
-    let adUnit = '';
-    if (this.adUnit) {
-      adUnit = `dc_iu=${this.adUnit}`;
+      let adUnit = '';
+      if (this.adUnit) {
+        adUnit = `dc_iu=${this.adUnit}`;
+      }
+
+      let ppid = '';
+      if (this.ppid) {
+        ppid = `ppid=${this.ppid}`;
+      }
+
+      const pixel = document.createElement('img');
+
+      pixel.src = 'https://pubads.g.doubleclick.net/activity;ord=';
+      pixel.src += `${random};dc_seg=${this.segmentId};${adUnit}${ppid}`;
+
+      pixel.width = 1;
+      pixel.height = 1;
+      pixel.border = '0';
+
+      pixel.style.visibility = 'hidden';
+
+      this.elementRef.nativeElement.append(pixel);
     }
-
-    let ppid = '';
-    if (this.ppid) {
-      ppid = `ppid=${this.ppid}`;
-    }
-
-    const pixel = document.createElement('img');
-
-    pixel.src = 'https://pubads.g.doubleclick.net/activity;ord=';
-    pixel.src += `${random};dc_seg=${this.segmentId};${adUnit}${ppid}`;
-
-    pixel.width = 1;
-    pixel.height = 1;
-    pixel.border = '0';
-
-    pixel.style.visibility = 'hidden';
-
-    this.elementRef.nativeElement.append(pixel);
   }
-
 }
