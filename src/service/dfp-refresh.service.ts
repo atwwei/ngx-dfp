74dfde84d3463e3ecfc2ba8e16a783aa27a24a12
atwwei <atwwei@gmail.com>
2018-07-18 09:17:16 +0800

    Unuse rxjs-compat

--- src/service/dfp-refresh.service.ts
+++ src/service/dfp-refresh.service.ts
@@ -1,9 +1,7 @@
 import { Injectable, EventEmitter, Optional, Injector, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
-import { Subscription } from 'rxjs';
-import { timer } from 'rxjs/observable/timer';
-import { from } from 'rxjs/observable/from';
+import { Subscription, timer, from } from 'rxjs';
 
 import { DfpConfig, DFP_CONFIG } from '../class';
 import { ParseDurationService } from './parse-duration.service';
