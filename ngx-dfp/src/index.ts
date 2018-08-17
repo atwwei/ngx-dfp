a36752c588271196dd5fb08a2164086ae569568a
atwwei <atwwei@gmail.com>
2018-08-17 12:40:44 +0800

    Update module exports

--- ngx-dfp/src/index.ts
+++ ngx-dfp/src/index.ts
@@ -1,12 +1,4 @@
-export { DFP_CONFIG } from './service/injection_token';
-export { IdleService } from './service/idle.service';
-export { HttpErrorService } from './service/http-error.service';
-export { ParseDurationService } from './service/parse-duration.service';
-export { ScriptInjectorService } from './service/script-injector.service';
-export { DfpService } from './service/dfp.service';
-export { DfpIDGeneratorService } from './service/dfp-id-generator.service';
-export { DfpRefreshService } from './service/dfp-refresh.service';
-
+export * from './service';
 export * from './directive';
 
 export { DfpModule } from './dfp.module';
