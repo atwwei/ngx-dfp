76798d7260646df633a33924425473bc6c2028c6
atwwei <atwwei@gmail.com>
2018-08-17 08:03:51 +0800

    Del unused files

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var prune = require('gulp-prune');
-
-// 清除Angular ngc临时文件
-gulp.task('default', () => {
-    return gulp.src(['lib/**', '!lib/**/*.ngfactory.*', '!lib/**/*.ngsummary.*'])
-        .pipe(prune('lib/'))
-});
