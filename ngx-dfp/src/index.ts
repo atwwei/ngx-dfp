32e571ce098fcb43ca2f1977dca374fee5a062ea
王卫 <atwwei@gmail.com>
2021-06-06 12:07:34 +0800

    Update

--- ngx-dfp/src/index.ts
+++ ngx-dfp/src/index.ts
@@ -1,4 +1,4 @@
-import './googletag.d';
+import './googletag';
 export * from './types';
 export { DfpAdDirective } from './directive/dfp-ad.directive';
 export { DfpService } from './service/dfp.service';
